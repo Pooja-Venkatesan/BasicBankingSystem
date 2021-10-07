cede19c1412d3ff8b6398588fe6f918dc5db850a
Pooja Venkatesan <poojavenkatesan011@gmail.com>
2021-10-07 14:53:26 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -6,16 +6,25 @@ document.getElementById("transButton").onclick = function() {
     location.href="TransferForm.html";
 };
 
+ 
 
-function send() {
-        
-    if (confirm("Confrim to make transaction!")) {
-        txt = "You pressed OK!";
-        alert("Transaction Successful!")
-    }else {
-        txt = "You pressed Cancel!";
-        alert("Transaction Cancelled!")
-      }
-    
+function send()
+{
+var empt = document.forms["form1"]["text1"].value;
+if (empt == "")
+{
+    alert('error please fill all fields!');
+}
+      else if (confirm("Confrim to make transaction!")) {
+    txt = "You pressed OK!";
+    alert("Transaction Successful!")
     location.href="history.html";
+}
+
+else {
+    txt = "You pressed Cancel!";
+    alert("Transaction Cancelled!")
+    location.href="history.html";
+  }
+
 };
